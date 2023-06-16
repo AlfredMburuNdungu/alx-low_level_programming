87e9d556706423ad22662e9296dcc5e8e24af3fb
AlfredMburuNdungu <alfredmburundungu@gmail.com>
2023-06-16 05:51:51 -0700

    edits

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,19 @@
-int main(void) {
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the lowercase alphabet excluding 'q' and 'e' followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
     char letter;
 
+    /**
+     * Print the lowercase alphabet excluding 'q' and 'e'
+     */
     for (letter = 'a'; letter <= 'z'; letter++) {
         if (letter != 'q' && letter != 'e') {
             putchar(letter);
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,23 @@
-int main(void) {
-    putchar('0');
-    putchar('1');
-    putchar('2');
-    putchar('3');
-    putchar('4');
-    putchar('5');
-    putchar('6');
-    putchar('7');
-    putchar('8');
-    putchar('9');
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints all single-digit numbers of base 10 starting from 0, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int number;
+
+    /**
+     * Print all single-digit numbers of base 10 starting from 0
+     */
+    for (number = 0; number <= 9; number++) {
+        putchar(number + '0');
+    }
+
     putchar('\n');
 
     return 0;
